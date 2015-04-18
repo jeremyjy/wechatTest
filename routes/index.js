2275cf5f76279b0ac1cacb90e91b28e195817a46
JeremyNg <jeremyjy@163.com>
2015-04-18 20:43:22 +0800

    xxx

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 var wechatApi = require('wechat-api')
 var api = new wechatApi('wx302ca370b5b95750', 'ce440bc4efc9f6ebe37d3380440a8745')
 var crypto = require('crypto')
-var xml2Json = require('xml2json')
-
+var xml2Json = require('xml2js')
+var builder = new xml2Json.Builder()
 
 
 /* GET home page. */
 
 router.get('/weixin', function (req, res, next) {
     var token = 'xxx'
-    var tmpArr = [token,req.query.timestamp,req.query.nonce]
+    var tmpArr = [token, req.query.timestamp, req.query.nonce]
     tmpArr.sort()
-    var sha1 = crypto.createHash ('sha1')
+    var sha1 = crypto.createHash('sha1')
     sha1.update(tmpArr.join(''))
     var gen = sha1.digest('hex')
     console.log(gen)
     console.log(req.query.signature)
-    if(gen!=req.query.signature){
+    if (gen != req.query.signature) {
         return false
         console.log(api)
     }
@@ -28,13 +28,16 @@ router.get('/weixin', function (req, res, next) {
 })
 router.post('/weixin', function (req, res, next) {
     console.log(req.body)
-        api.sendText(req.body.fromusername, 'welcome!', function (err, result) {
-            if(err)
-            console.log(err)
-            else
-            console.log(result)
-            next()
-        })
+    var mess = {
+        xml: {
+            tousername:res.body.xml.fromusername,
+            fromeusername:res.body.xml.tousername,
+            msgtype:'text',
+            content:'welcome',
+            createtime:new Date().getTime()
+        }
+    }
+    res.send(builder.buildObject(mess))
 })
 
 module.exports = router;
